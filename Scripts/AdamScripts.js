394cd573d06bb6a5d25763c2ed7c0094db0b41d9
Adam Brooks <52460829+atbrooks84@users.noreply.github.com>
2020-10-17 11:51:28 -0400

    working on the form

--- Scripts/AdamScripts.js
+++ Scripts/AdamScripts.js
@@ -1,70 +1,83 @@
 ﻿const simpleInterest = document.querySelector('.simple-interest');
 const button = document.querySelector('.button');
 //const loading = document.querySelector('.loader');
-const results = document.querySelector('.results');
+const results = document.querySelector('.result');
+
+document.getElementById("simple-interest").addEventListener('submit', calculateResults())
 
 function calculateResults(e) {
-	// ui elements
-	const principal = document.querySelector('#principal');
-	const rate = document.querySelector('#rate');
-	const time = document.querySelector('#years');
-	const months =document.querySelector('#months')
-	const monthlyPayment = document.querySelector('#payment');
-	const totalInterest = document.querySelector('#interest');
-	const totalAmount = document.querySelector('#total');
-	// formula variables
-	const p = parseFloat(principal.value);
-	const r = parseFloat(rate.value);
-	const t = parseFloat(time.value);
 
-	// calculate total interest
-	const interest = (p * t * r / 100);
-	// calculate monthly payment
-	const payment = ((interest + p) / (t * 12)).toFixed(2);
-	// calculate total amount paid
-	const total = (interest + p).toFixed(2);
+    document.getElementById("test").value = "test";
+    // ui elements
+    const principal = document.querySelector('#principal');
+    const rate = document.querySelector('#rate');
+    const years = document.querySelector('#years');
+    const months = document.querySelector('#months')
+    const monthlyPayment = document.querySelector('#payment');
+    const totalInterest = document.querySelector('#interest');
+    const totalAmount = document.querySelector('#total');
+    // formula variables
+    const p = parseFloat(principal.value);
+    const r = parseFloat(rate.value);
+    if (time > 0) {
+        const t = parseFloat(years.value);
+    }
+    else if (months > 0) {
+        const t = parseFloat(months.value);
+    }
+    else {
+        showError("You did not enter any time")
+    }
 
-	if (isFinite(payment)) {
-		totalInterest.innerHTML = '$' + (interest).toFixed(2);
-		monthlyPayment.innerHTML = '$' + payment;
-		totalAmount.innerHTML = '$' + total;
-		// hide loader
-		button.classList.remove('loading');
-		// show results
-		results.classList.remove('hide');
-	} else {
-		// show error
-		showError('Please check your numbers and try again.');
-		// hide loader
-		button.classList.remove('loading');
-	}
+
+    // calculate total interest
+    const interest = (p * t * r / 100);
+    // calculate monthly payment
+    const payment = ((interest + p) / (t * 12)).toFixed(2);
+    // calculate total amount paid
+    const total = (interest + p).toFixed(2);
+
+    if (isFinite(payment)) {
+        totalInterest.innerHTML = '$' + (interest).toFixed(2);
+        monthlyPayment.innerHTML = '$' + payment;
+        totalAmount.innerHTML = '$' + total;
+        // hide loader
+        button.classList.remove('loading');
+        // show results
+        results.classList.remove('hide');
+    } else {
+        // show error
+        showError('Please check your numbers and try again.');
+        // hide loader
+        button.classList.remove('loading');
+    }
 }
 
 function showError(error) {
-	// create error
-	const errorMessage = document.createElement('div');
-	const calculate = document.querySelector('#calculate');
+    // create error
+    const errorMessage = document.createElement('div');
+    const calculate = document.querySelector('#calculate');
 
-	errorMessage.className = 'error';
-	errorMessage.appendChild(document.createTextNode(error));
-	simpleInterest.insertBefore(errorMessage, calculate);
-	// clear error
-	setTimeout(clearError, 3000);
+    errorMessage.className = 'error';
+    errorMessage.appendChild(document.createTextNode(error));
+    simpleInterest.insertBefore(errorMessage, calculate);
+    // clear error
+    setTimeout(clearError, 3000);
 }
 
 function clearError() {
-	// remove error
-	document.querySelector('.error').remove();
+    // remove error
+    document.querySelector('.error').remove();
 }
 
 button.addEventListener('click', (e) => {
-	console.log('Calculating...');
-	// show loader
-	button.classList.add('loading');
+    console.log('Calculating...');
+    // show loader
+    button.classList.add('loading');
 
-	// set timeout
-	setTimeout(calculateResults, 2000);
+    // set timeout
+    setTimeout(calculateResults, 2000);
 
-	// prevent page from reloading on submit
-	e.preventDefault();
+    // prevent page from reloading on submit
+    e.preventDefault();
 });
