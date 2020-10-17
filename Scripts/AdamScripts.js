d7df228a11d119a2c306c900d02ef40cb8a8f14f
Adam Brooks <52460829+atbrooks84@users.noreply.github.com>
2020-10-17 15:25:56 -0400

    form works

--- Scripts/AdamScripts.js
+++ Scripts/AdamScripts.js
@@ -7,53 +7,58 @@ const results = document.querySelector('.result');
 
 function calculateResults(e) {
 
-    
+
     // ui elements
     const principal = Number(document.getElementById("principal").value);
     const rate = Number(document.getElementById("interest").value);
     const years = Number(document.getElementById("years").value);
     const months = Number(document.getElementById("months").value);
-    const monthlyPayment = document.getElementById("payment").innerHTML;
-    const totalInterest = document.querySelector('#interest');
-    const totalAmount = document.querySelector('#total');
-    // formula variables
-    const p = parseFloat(principal.value);
-    const r = parseFloat(rate.value);
-    if (years > 0) {
-        const t = parseFloat(years.value);
-    }
-    else if (months > 0) {
-        const t = parseFloat(months.value);
-    }
-    else {
-        showError("You did not enter any time")
-    }
 
-    if (principal > 0) {
-        document.getElementById("test").innerHTML = "test";
-    }
-    // calculate total interest
-    const interest = (p * t * r / 100);
-    // calculate monthly payment
-    const payment = ((interest + p) / (t * 12)).toFixed(2);
-    // calculate total amount paid
-    const total = (interest + p).toFixed(2);
+    let testOutput = "$100";
+    document.getElementById("payment").innerHTML = testOutput;
 
-    if (isFinite(payment)) {
-        totalInterest.innerHTML = '$' + (interest).toFixed(2);
-        monthlyPayment.innerHTML = '$' + payment;
-        totalAmount.innerHTML = '$' + total;
-        // hide loader
-        button.classList.remove('loading');
-        // show results
-        results.classList.remove('hide');
-    } else {
-        // show error
-        showError('Please check your numbers and try again.');
-        // hide loader
-        button.classList.remove('loading');
-    }
 }
+//    const monthlyPayment = document.getElementById("payment");
+//    const totalInterest = document.querySelector('#interest');
+//    const totalAmount = document.querySelector('#total');
+//    // formula variables
+//    const p = parseFloat(principal.value);
+//    const r = parseFloat(rate.value);
+//    if (years > 0) {
+//        const t = parseFloat(years.value);
+//    }
+//    else if (months > 0) {
+//        const t = parseFloat(months.value);
+//    }
+//    else {
+//        showError("You did not enter any time")
+//    }
+
+//    if (principal > 0) {
+//        document.getElementById("test").innerHTML = "test";
+//    }
+//    // calculate total interest
+//    const interest = (p * t * r / 100);
+//    // calculate monthly payment
+//    const payment = ((interest + p) / (t * 12)).toFixed(2);
+//    // calculate total amount paid
+//    const total = (interest + p).toFixed(2);
+
+//    if (isFinite(payment)) {
+//        totalInterest.innerHTML = '$' + (interest).toFixed(2);
+//        monthlyPayment.innerHTML = '$' + payment;
+//        totalAmount.innerHTML = '$' + total;
+//        // hide loader
+//        button.classList.remove('loading');
+//        // show results
+//        results.classList.remove('hide');
+//    } else {
+//        // show error
+//        showError('Please check your numbers and try again.');
+//        // hide loader
+//        button.classList.remove('loading');
+//    }
+//}
 
 function showError(error) {
     // create error
