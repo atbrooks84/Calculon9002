98655e6d0144b6771ee91cca89089d608b130f05
Joshua Peters <60118191+joshpters@users.noreply.github.com>
2020-10-17 14:06:09 -0400

    Updated layout
    Updated layout structure to now have the input and output containers on top of each other instead of side-by-side.

--- Scripts/Custom.js
+++ Scripts/Custom.js
@@ -7,28 +7,25 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: 'Monthly Payments',
             data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(225, 78, 202, 0.2)',
-                //'rgba(54, 162, 235, 0.2)',
-                //'rgba(255, 206, 86, 0.2)',
-                //'rgba(75, 192, 192, 0.2)',
-                //'rgba(153, 102, 255, 0.2)',
-                //'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(225, 78, 202, 1)',
-                //'rgba(54, 162, 235, 1)',
-                //'rgba(255, 206, 86, 1)',
-                //'rgba(75, 192, 192, 1)',
-                //'rgba(153, 102, 255, 1)',
-                //'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            backgroundColor: 'rgba(225, 78, 202, 0.2)',
+            borderColor: 'rgba(225, 78, 202, 1)',
+            borderWidth: 1,
+            pointHitRadius: 20
         }]
     },
     options: {
         scales: {
+            xAxes: [{
+                display: true,
+                gridLines: {
+                    color: "#383838"
+                },
+            }],
             yAxes: [{
+                display: true,
+                gridLines: {
+                    color: "#383838"
+                },
                 ticks: {
                     beginAtZero: true
                 }
