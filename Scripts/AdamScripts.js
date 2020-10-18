e8cd3457e0c588f3aec6debacd5a33998b6ee2ca
Joshua Peters <60118191+joshpters@users.noreply.github.com>
2020-10-17 20:33:34 -0400

    Removed validation code to test function
    Temporarily removed validation and input variables to test core function

--- Scripts/AdamScripts.js
+++ Scripts/AdamScripts.js
@@ -2,74 +2,103 @@
 
 
     // ui elements
-    const principal = Number(document.getElementById("principal").value);
-    document.getElementById("initial").innerHTML = "$" + principal;
-    const interest = Number(document.getElementById("interest").value);
-    let years = Number(document.getElementById("years").value);
-    let months = Number(document.getElementById("months").value);
+
+    //const principal = Number(document.getElementById("principal").value);
+    //document.getElementById("initial").innerHTML = "$" + principal;
+    //const interest = Number(document.getElementById("interest").value);
+    //let years = Number(document.getElementById("years").value);
+    //let months = Number(document.getElementById("months").value);
 
 
 
 
+    const loanAmount = 5000;
+    const interest = 5;
+    const months = 120;
 
+    //if (years && months) {
+    //    showError("Please only use months or years")
+    //}
+    //else if (years > 0) {
+    //    months = years * 12;
+    //    let loanAmount = parseFloat(principal);
+    //    let rate = parseFloat(interest);
+    //    var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
+    //    document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
 
-    if (years && months) {
-        showError("Please only use months or years")
-    }
-    else if (years > 0) {
-        months = years * 12;
-        let loanAmount = parseFloat(principal);
-        let rate = parseFloat(interest);
-        var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
-        document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
-
-    }
-    else if (months > 0) {
+    //}
+    //else if (months > 0) {
         
-        let loanAmount = parseFloat(principal);
-        let rate = parseFloat(interest);
-        var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
-        document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
+    //    let loanAmount = parseFloat(principal);
+    //    let rate = parseFloat(interest);
+    //    var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
+    //    document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
 
+    //}
+    //else {
+    //    showError("You didn't enter a time limit");
+    //    return;
+    //}
+
+
+
+
+    let monthlyPayment = (loanAmount) * (interest / 1200) / (1 - (1 + interest / 1200) ** (-months));
+    let remainingBalance = loanAmount;
+    let amortizationSchedule = []
+
+    for (let i = 0; i < months; i++) {
+        interestPayment = remainingBalance * (interest / 1200);
+        principalPayment = monthlyPayment - interestPayment
+        console.log(remainingBalance, principalPayment)
+        remainingBalance = remainingBalance - principalPayment;
+
+        //if (remainingBalance < monthlyPayment) {
+        //    interestPayment = 0;
+        //    principalPayment = remainingBalance
+        //}
+
+        amortizationSchedule.push([i + 1, monthlyPayment, principalPayment, interestPayment, remainingBalance])
+        console.log(...amortizationSchedule[i])
     }
-    else {
-        showError("You didn't enter a time limit");
-        return;
-    }
 
 
-    
 
 
-    paymentsPrincipal = [];
-    paymentsPrincipal.push(parseFloat(principal));
+    //let paymentsPrincipal = [];
+    //let paymentsInterest = [];
     //let previousRemainingBalance = principal;
     //let currentInterest = 0;
-    for (let i = 0, paymentsInterest = [], previousRemainingBalance = principal, currentInterest = 0; i < months; i++) {
+    //paymentsPrincipal.push(parseFloat(principal));
+    ////let previousRemainingBalance = principal;
+    ////let currentInterest = 0;
+    //for (let i = 0; i < months; i++) {
 
-        //let previousRemainingBalance = principal;
+    //    //let previousRemainingBalance = principal;
         
         
-        //let currentInterest = 0;
-        let rate = parseFloat(interest);
-        let interestPayment = ((previousRemainingBalance * rate) / 1200);        
-        let principalPayment = parseFloat(monthlyPayment) - parseFloat(interestPayment);
-        let updateTotal = parseFloat(interestPayment) + parseFloat(currentInterest);
-        currentInterest = parseFloat(updateTotal);
-        previousRemainingBalance = previousRemainingBalance - monthlyPayment;
-        paymentsInterest.push(parseFloat(interestPayment));
-        paymentsPrincipal.push(parseFloat(principalPayment));
-        let finalTotal = parseFloat(principal) + parseFloat(updateTotal);
+    //    //let currentInterest = 0;
+    //    let rate = interest;
+    //    let interestPayment = ((previousRemainingBalance * rate) / 1200);        
+    //    let principalPayment = monthlyPayment - interestPayment;
+    //    let updateTotal = interestPayment + currentInterest;
+    //    currentInterest = updateTotal;
+    //    previousRemainingBalance = previousRemainingBalance - monthlyPayment;
+    //    paymentsInterest.push(interestPayment);
+    //    paymentsPrincipal.push(principalPayment);
+    //    let finalTotal = principal + updateTotal;
 
-        document.getElementById("testInterestPayments").innerHTML = paymentsInterest.join(", $");
-        document.getElementById("testPrincipalPayments").innerHTML = paymentsPrincipal.join(", $");
-        document.getElementById("finalInterest").innerHTML = parseFloat(updateTotal).toFixed(2);
-        document.getElementById("total").innerHTML = finalTotal;
-    }
+    //    document.getElementById("testInterestPayments").innerHTML = paymentsInterest.join(", $");
+    //    document.getElementById("testPrincipalPayments").innerHTML = paymentsPrincipal.join(", $");
+    //    document.getElementById("finalInterest").innerHTML = parseFloat(updateTotal).toFixed(2);
+    //    document.getElementById("total").innerHTML = finalTotal;
+    //}
 
    
 
 }
+
+calculateResults("");
 
 
     //function showError(error) {
