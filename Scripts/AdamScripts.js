68613c24ddb0d5583aa3baad3fcabc844fd6838d
Joshua Peters <60118191+joshpters@users.noreply.github.com>
2020-10-18 12:49:05 -0400

    Removed commented code

--- Scripts/AdamScripts.js
+++ Scripts/AdamScripts.js
@@ -1,208 +1,70 @@
-﻿function calculateResults(e) {
+﻿const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+})
+
+function generateGrid(amortizationSchedule) {
+    html = "";
+    for (let row = 0; row < amortizationSchedule.length; row++) {
+        html += "<tr>";
+        html += `<th scope="row">${amortizationSchedule[row][0]}</th>`
+        for (let cell = 1; cell < amortizationSchedule[row].length; cell++) {
+            html += `<td>${formatter.format(amortizationSchedule[row][cell].toFixed(2))}</td>`
+        }
+
+        html += "</tr>";
+    }
+
+    document.getElementById("generated").innerHTML = html;
+}
 
 
-    // ui elements
-
+function calculateResults(e) {
     const principal = Number(document.getElementById("principal").value);
     document.getElementById("initial").innerHTML = "$" + principal;
     const interest = Number(document.getElementById("interest").value);
     let years = Number(document.getElementById("years").value);
     let months = Number(document.getElementById("months").value);
 
-
-
-
-    //const loanAmount = 5000;
-    //const interest = 5;
-    //const months = 120;
-
-    if (years && months) {
-        showError("Please only use months or years")
-    }
-    else if (years > 0 && years < 50) {
-        months = years * 12;
-        let loanAmount = parseFloat(principal);
-        let rate = parseFloat(interest);
-        var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
-        document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
-
-    }
-    else if (months > 0 && months < 600) {
-        
-        let loanAmount = parseFloat(principal);
-        let rate = parseFloat(interest);
-        var monthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-months));
-        document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
-
-    }
-    else if (years > 50 || months > 600) {
-        showError("That's too long")
-    }
-    else {
-        showError("You didn't enter a time limit");
+    if (!years && !months) {
+        showError("Please enter a time limit");
         return;
     }
 
+    if (years) {
+        months = years * 12;
+    }
 
+    if (months > 600) {
+        showError("Too many years. Please enter 50 or less.")
+    }
 
-    let loanAmount = principal;
-    //let monthlyPayment = (loanAmount) * (interest / 1200) / (1 - (1 + interest / 1200) ** (-months));
-    let remainingBalance = loanAmount;
-    let amortizationSchedule = []
-    let paymentsInterest = [];
-    let paymentsPrincipal = [];
-    let interestPaymentsSum = [];
-    let totalInterestCount = [];
-    let remainingBalanceCount = [];
+    let totalInterest = 0;
+    let monthlyPayment = (principal) * (interest / 1200) / (1 - (1 + interest / 1200) ** (-months));
+    let remainingBalance = principal;
+    let amortizationSchedule = [];
 
-    for (let i = 0, currentInterest = 0, totalInterest = 0; i < months; i++) {
+    document.getElementById("payment").innerHTML = monthlyPayment.toFixed(2);
+
+    for (let i = 0; i < months; i++) {
         let interestPayment = remainingBalance * (interest / 1200);
         let principalPayment = monthlyPayment - interestPayment
 
+        totalInterest += interestPayment;
         remainingBalance = remainingBalance - principalPayment;
 
-        amortizationSchedule.push([i + 1, monthlyPayment, principalPayment, interestPayment, remainingBalance])
-        //console.log(...amortizationSchedule[i])
-        paymentsInterest.push(interestPayment);
-        paymentsPrincipal.push(principalPayment)
-        interestPaymentsSum.push(interestPayment)
-        remainingBalanceCount.push(remainingBalance);
-
-        function sum(total, num) {
-            return total + num;
-        }
-
-        let finalInterest = interestPaymentsSum.reduce(sum).toFixed(2);
-        let totalLoan = parseFloat(finalInterest) + principal;
-
-        //document.getElementById("testInterestResults").innerHTML = interestPaymentsSum.join(", $");
-        document.getElementById("finalInterest").innerHTML = finalInterest;
-        document.getElementById("total").innerHTML = totalLoan;
-
-        function generateGrid(amortizationSchedule) {
-            html = "";
-            for (let row = 0; row < amortizationSchedule.length; row++) {
-                html += "<tr>";
-                html += `<th scope="row">${amortizationSchedule[row][0]}</th>`
-                for (let cell = 1; cell < amortizationSchedule[row].length; cell++) {
-                    html += `<td>${amortizationSchedule[row][cell]}</td>`
-                }
-
-                html += "</tr>";
-            }
-
-            document.getElementById("generated").innerHTML = html;
-        }
-
-        generateGrid(amortizationSchedule);
-        
-        //totalInterest = currentInterest + interestPayment;
-        //currentInterest = interestPayment;
-        //finalCost = principal + totalInterest;
-
-        
-
-        //document.getElementById("testInterestPayments").innerHTML = paymentsInterest.join(", $");
-        //document.getElementById("testPrincipalPayments").innerHTML = paymentsPrincipal.join(", $");
-        //document.getElementById("testBalanceResults").innerHTML = remainingBalanceCount.join(", $");
-        
-        //document.getElementById("finalInterest").innerHTML = parseFloat(totalInterest).toFixed(2);
-        //document.getElementById("total").innerHTML = finalCost;
-
-        
-        //for (let a = 0, sum = 0; a < interestPaymentsSum.length; a++) {
-        //    sum += parseFloat(interestPaymentsSum[a]);
-        //    totalInterestCount.push(sum);
-        //    finalCost = principal + sum;
-            
-        //    //document.getElementById("testInterestResults").innerHTML = totalInterestCount.join(", $");
-            
-
-
-        //    document.getElementById("finalInterest").innerHTML = parseFloat(sum).toFixed(2);
-        //    document.getElementById("total").innerHTML = finalCost.toFixed(2) ;
-        //}
-            
+        amortizationSchedule.push([i + 1, monthlyPayment, principalPayment, interestPayment, totalInterest, remainingBalance])
     }
 
     
+    generateGrid(amortizationSchedule);
 
-
-
-
-
-    //let paymentsPrincipal = [];
-    //let paymentsInterest = [];
     paymentsPrincipal = [];    
-    //let previousRemainingBalance = principal;
-    //let currentInterest = 0;
-    //paymentsPrincipal.push(parseFloat(principal));
-    ////let previousRemainingBalance = principal;
-    ////let currentInterest = 0;
-    //for (let i = 0; i < months; i++) {
-
-    //    //let previousRemainingBalance = principal;
-
-    //    //let currentInterest = 0;
-    //    let rate = interest;
-    //    let interestPayment = ((previousRemainingBalance * rate) / 1200);        
-    //    let principalPayment = monthlyPayment - interestPayment;
-    //    let updateTotal = interestPayment + currentInterest;
-    //    currentInterest = updateTotal;
-    //    previousRemainingBalance = previousRemainingBalance - monthlyPayment;
-    //    paymentsInterest.push(interestPayment);
-    //    paymentsPrincipal.push(principalPayment);
-    //    let finalTotal = principal + updateTotal;
-
-    //    document.getElementById("testInterestPayments").innerHTML = paymentsInterest.join(", $");
-    //    document.getElementById("testPrincipalPayments").innerHTML = paymentsPrincipal.join(", $");
-    //    document.getElementById("finalInterest").innerHTML = parseFloat(updateTotal).toFixed(2);
-    //    document.getElementById("total").innerHTML = finalTotal;
-    //}
-
-        //let currentInterest = 0;
-        //let rate = parseFloat(interest);
-        //let interestPayment = ((previousRemainingBalance * (rate / 1200)));        
-        //let principalPayment = parseFloat(monthlyPayment) - parseFloat(interestPayment);
-        //let updateTotal = parseFloat(interestPayment) + parseFloat(currentInterest);
-        //currentInterest = parseFloat(updateTotal);
-        //previousRemainingBalance = previousRemainingBalance - monthlyPayment;
-        //paymentsInterest.push(parseFloat(interestPayment));
-        //paymentsPrincipal.push(parseFloat(principalPayment));
-        //let finalTotal = parseFloat(principal) + parseFloat(updateTotal);
-
-        //document.getElementById("testInterestPayments").innerHTML = paymentsInterest.join(", $");
-        //document.getElementById("testPrincipalPayments").innerHTML = paymentsPrincipal.join(", $");
-        //document.getElementById("finalInterest").innerHTML = parseFloat(updateTotal).toFixed(2);
-        //document.getElementById("total").innerHTML = finalTotal;
-    }
+}
 
 
-   
-
-
-
-calculateResults("");
-
-
-    function showError(error) {
-        // create error
-        document.getElementById("error").innerHTML = error;
-    }
-
-    //function clearError() {
-    //    // remove error
-    //    document.querySelector('.error').remove();
-    //}
-
-    //button.addEventListener('click', (e) => {
-    //    console.log('Calculating...');
-    //    // show loader
-    //    button.classList.add('loading');
-
-    //    // set timeout
-    //    setTimeout(calculateResults, 2000);
-
-    //    // prevent page from reloading on submit
-    //    e.preventDefault();
-    //});
+function showError(error) {
+    // create error
+    document.getElementById("error").innerHTML = error;
+}
